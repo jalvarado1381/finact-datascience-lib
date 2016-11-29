b48d13cd1ede69f4c7123456e7fc5b8a1705543c
Juan Alvarado <juan.alvarado.1381@gmail.com>
2016-11-29 15:55:55 -0400

    Adding list: monthsList and monthsNumber

--- static-libs/finact-datascience-lib-static/date.cpp
+++ static-libs/finact-datascience-lib-static/date.cpp
@@ -3,13 +3,27 @@
 
 
         Date::Date(){
-            setDay(0), setMonth(0), setYear(0);
+            setDay(1), setMonth(1), setYear(1);
+            monthsNumber = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+            monthsNamesEN = {"January","February","March","April","May","June",
+            "July","August","September","October","November","December"};
+            monthsNamesES = {"Enero","Febrero","Marzo","Abril","Mayo","Junio",
+            "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+            monthsList.push_back(monthsNumber);
+            monthsList.push_back(monthsNamesEN);
+            monthsList.push_back(monthsNamesES);
         }
         Date::Date( int d, int m, int y){
             setDay(d), setMonth(m), setYear(y);
         }
         void Date::setDay( int d ){
-            day = d;
+            if ( validateDay(d) ){
+                day = d;
+            }
+            else{
+                cerr << "Invalid day number (" << d << ")." <<endl;
+                exit(-1);
+            }
         }
         void Date::setMonth( int m ){
             month = m;
@@ -23,9 +37,19 @@
             setMonth(m);
             setYear(y);
         }
-
         bool Date::validateDay(int d ){
             //monthDaysRanges.
+            bool value = false;
+            if ( d > 0 )
+                for (auto i : monthDaysRanges){
+                    if (d <= i ){
+                        value = true;
+                        break;
+                    }
+                    else
+                        continue;
+                }
+            return value;
         }
         bool Date::validateMonth(int m ){}
         bool Date::validateYear(int y){}
--- static-libs/finact-datascience-lib-static/date.h
+++ static-libs/finact-datascience-lib-static/date.h
@@ -12,12 +12,14 @@ class Date {
     private:
         list<int> monthDaysRanges = {28,29,30,31};
         list<int> yearDayRanges = {360, 365, 366};
-        const int yearMonths = 12;
-        list<string> monthsNamesEN = {"January","February","March","April","May","June",
-        "July","August","September","October","November","December"};
-        list<string> monthsNamesES = {"Enero","Febrero","Marzo","Abril","Mayo","Junio",
-        "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+        const int monthsPerYear = 12;
+        list<string> monthsNumber;
+        list<string> monthsNamesEN ;
+        list<string> monthsNamesES ;
+        std::list<list<string>> monthsList;
+
         int day, month, year;
+
         bool validateDay(int);
         bool validateMonth(int);
         bool validateYear(int);
